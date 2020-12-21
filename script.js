fb81d67d835716cca41f9db160b12549d45639ee
Rasmus Thinghuus Toft <rasmustoft@Rasmuss-MacBook-Pro.local>
2020-12-21 11:43:50 +0100

    initial commit

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-//Burgermenu
-
-
+//window.onscroll = function () {
+//    myFunction()
+//};
+//
+//var web_menu = document.getElementById("#web_menu");
+//
+//var sticky = navbar.offsetTop;
+//
+//
+//function myFunction() {
+//    if (window.pageYOffset >= sticky) {
+//        web_menu.classList.add("sticky")
+//    } else {
+//        web_menu.classList.remove("sticky");
+//    }
+//}
